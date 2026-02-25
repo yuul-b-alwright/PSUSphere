31b0b9913f8a0b985d09d0df3f80fce79869d505
Fernandez <202180420@psu.palawan.edu.ph>
2026-02-25 23:49:58 +0800

    added Bootstrap

--- PSUSphere/projectsite/settings.py
+++ PSUSphere/projectsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'studentorg',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'projectsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'projectsite' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,4 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'projectsite' / 'static',
+]
--- PSUSphere/studentorg/views.py
+++ PSUSphere/studentorg/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
 
-# Create your views here.
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'organizations'
+    template_name = 'home.html'
+    paginate_by = 10
+
